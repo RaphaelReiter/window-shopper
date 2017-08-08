6e310489376202dc915663307000a3c483d6eb7a
Raphaël Reiter <raphaelreiter@Raphaels-iMac.local>
2017-08-08 13:57:06 +0200

    with Unit testing

---
+++ window-shopper/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Raphaël Reiter on 07/08/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ window-shopper/Wage.swift
@@ -0,0 +1,9 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Raphaël Reiter on 08/08/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import Foundation
